48ef2d4f4fcd3cc0132ee9b6aa4aec34221bfaa6
detayaa <drugochenko.taya@mail.ru>
2022-09-19 23:22:24 +0700

    1

--- src/main.java
+++ src/main.java
@@ -1,5 +1,24 @@
 public class main {
-    public static void main(String[] args) {
 
+    int[] arr = generateRandomArray();
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+            }
+        return arr;
     }
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray();
+        System.out.println("Задание 1");
+        int summ = 0;
+        for (int i = 0; i < arr.length; i++) {
+            summ += arr[i];
+        }
+        System.out.println(summ);
+    }
+
 }
+
